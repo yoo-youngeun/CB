15e5b1aa025b5e9486c02bcf0e53f9c331f57c79
Administrator <Administrator@WIN-T56GM6LP5JC.localdomain>
2024-05-13 14:19:50 +0900

    "#2180 commit test"

---
+++ src/cbTest/com/intland/codebeamer/TestClass.java
@@ -0,0 +1,10 @@
+package cbTest.com.intland.codebeamer;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
