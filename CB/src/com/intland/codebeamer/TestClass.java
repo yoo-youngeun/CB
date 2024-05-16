1fd4f77a087c0ea2a2cacabe4ce2939ce78ff681
yeyoo <yeyoo@zerosilver>
2024-05-16 16:26:07 +0900

    "#1000 test"

---
+++ CB/src/com/intland/codebeamer/TestClass.java
@@ -0,0 +1,10 @@
+package com.intland.codebeamer;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("zzzzzz");
+	}
+
+}
--- test/src/test/com/intland/codebeamer/controller/TestController.java
+++
@@ -1,7 +0,0 @@
-package test.com.intland.codebeamer.controller;
-
-public class TestController {
-	public static void main(String[] args) {
-		System.out.println("zzzzdddddzz");
-	}
-}
